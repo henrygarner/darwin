15ccd09a59705c2abe73487771ff59b5a1ab5a6e
Henry Garner <henry@henrygarner.com>
2013-02-06 20:04:06 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to darwin
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/darwin/core_test.clj
@@ -0,0 +1,7 @@
+(ns darwin.core-test
+  (:use clojure.test
+        darwin.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
